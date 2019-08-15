47d8e7fcd7299c7c5ac166bcd04f6e8bef47e781
Brad Engelhardt <brad.engelhardt@gmail.com>
2019-08-15 00:09:24 -0700

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../scss/App.scss';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>bradprints</h1>
+                <a href="https://www.instagram.com/bradprints/" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
+            </header>
+        </div>
+  );
+}
+
+export default App;
