7640b30840d8b9c840cf917bcf79cafdc06caf6a
Brad Engelhardt <brad.engelhardt@gmail.com>
2019-08-28 23:03:34 -0700

    move particle params to separate file

---
+++ src/config/particle.js
@@ -0,0 +1,43 @@
+module.exports = {
+    "particles": {
+        "number": {
+            "value": 150,
+            "density": {
+                "enable": true,
+                "value_area": 2500
+            }
+        },
+        "line_linked": {
+            "enable": true,
+            "opacity": 0.04
+        },
+        "move": {
+            "direction": "right",
+            "speed": 0.05
+        },
+        "size": {
+            "value": 1
+        },
+        "opacity": {
+            "anim": {
+                "enable": true,
+                "speed": 1,
+                "opacity_min": 0.05
+            }
+        }
+    },
+    "interactivity": {
+        "events": {
+            "onclick": {
+                "enable": true,
+                "mode": "push"
+            }
+        },
+        "modes": {
+            "push": {
+                "particles_nb": 1
+            }
+        }
+    },
+    "retina_detect": true
+}
